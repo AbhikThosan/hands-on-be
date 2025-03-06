5bf48862d615050f40403349c2144ca0a601f178
AbhikThosan <abhiknaththosan@gmail.com>
2025-03-06 23:33:02 +0600

    feat/auth
    login, registration, JWT token authorization, RBAC, update profile info, role update

---
+++ database/migrations/03062025_create_users_table.js
@@ -0,0 +1,30 @@
+const pool = require("../db");
+
+const createUsersTable = async () => {
+  const query = `
+    CREATE TABLE IF NOT EXISTS users (
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(100) NOT NULL,
+      email VARCHAR(100) UNIQUE NOT NULL,
+      password TEXT NOT NULL,
+      skills TEXT[],
+      causes_supported TEXT[],
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      volunteer_hours INTEGER DEFAULT 0,
+      volunteer_history JSONB DEFAULT '[]',
+      total_contributions JSONB DEFAULT '{}',
+      role VARCHAR(50) NOT NULL DEFAULT 'volunteer'
+    );
+  `;
+
+  try {
+    await pool.query(query);
+    console.log("✅ Users table created successfully!");
+  } catch (err) {
+    console.error("❌ Error creating users table:", err);
+  } finally {
+    pool.end();
+  }
+};
+
+createUsersTable();
